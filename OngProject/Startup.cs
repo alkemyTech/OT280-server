8b63ff8cbc5baf4ca7cd416c521bb0ab31bb92d2
MatiasAcostaa <matiasacostatuc@gmail.com>
2022-08-19 16:38:27 -0300

    Conection String y DbContext

--- OngProject/Startup.cs
+++ OngProject/Startup.cs
@@ -11,6 +11,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OngProject.DataAccess;
 
 namespace OngProject
 {
@@ -32,6 +34,9 @@ namespace OngProject
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "OngProject", Version = "v1" });
             });
+            
+            services.AddDbContext<AppDbContext>(ops =>
+                ops.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
