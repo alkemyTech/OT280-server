faf3c8100887d19ad3b1cc13c0e55b590e14be4a
Arturo-Castro <castro.ah91@gmail.com>
2022-08-21 17:40:14 -0300

    Add repository pattern
    Added repository, unit of work and controller for Member class.

---
+++ OngProject/DataAccess/Generic/GenericRepository.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OngProject.DataAccess.Generic
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        ValueTask<TEntity> GetByIdAsync(int id);
+        Task<bool> AddAsync(TEntity entity);
+        Task<bool> Delete(TEntity entity);
+    }
+
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GenericRepository(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork; 
+        }
+
+
+        public async Task<bool> AddAsync(TEntity entity)
+        {
+            bool created = false;
+
+            try
+            {
+                var save = await _unitOfWork.Context.Set<TEntity>().AddAsync(entity);
+
+                if (save != null)
+                    created = true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return created;
+        }
+
+        public Task<bool> Delete(TEntity entity)
+        {
+            bool deleted = false;
+
+            try
+            {
+                var remove =  _unitOfWork.Context.Set<TEntity>().Remove(entity);
+
+                if (remove != null)
+                    deleted = true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return Task.FromResult(deleted);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _unitOfWork.Context.Set<TEntity>().ToListAsync();
+        }
+
+        public async ValueTask<TEntity> GetByIdAsync(int id)
+        {
+            return await _unitOfWork.Context.Set<TEntity>().FindAsync(id);
+        }
+    }
+}
---
+++ OngProject/DataAccess/Generic/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace OngProject.DataAccess.Generic
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        AppDbContext Context { get; }
+        void commit();
+    }
+
+    public class UnitOfWork : IUnitOfWork
+    {
+        public AppDbContext Context { get; }
+
+        public UnitOfWork(AppDbContext context)
+        {
+            Context = context;
+        }
+
+        public void commit()
+        {
+            Context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            Context.Dispose();
+        }
+    }
+}
