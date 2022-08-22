0a0676e7beaffc89740fcc6b820f77159666a6b4
Diego Balderrama <diego.balde.job@gmail.com>
2022-08-21 21:11:58 -0300

    OT280-18-OT280-17- Added Users and Roles models and Identity dependencies

---
+++ OngProject/Core/Models/Users.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Net.Mail;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace OngProject.Entities.Domain
+{
+    [Index(nameof(Email), IsUnique = true)]
+    public class Users : IdentityUser
+    {
+        [Required(ErrorMessage = "First Name is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        public string LastName { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Email is required")]
+        override public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+        [Column(TypeName = "varchar(max)")]
+        public string Photo { get; set; }
+
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+        public bool IsDeleted { get; set; }
+
+    }
+}
