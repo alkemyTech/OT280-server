935272ea6e9352213b4f229d102e5d7d1f1fd6e5
Magali Medina <magalimedina96@gmail.com>
2022-08-23 11:47:58 -0300

    Se agrego accountControler y loginUserDTO

---
+++ OngProject/Controllers/AccountController.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OngProject.Core.Models;
+using OngProject.Core.Models.DTOs;
+using System;
+using System.Threading.Tasks;
+
+namespace OngProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<Users> _userManager;
+        private readonly SignInManager<Users> _signInManager;
+        private readonly ILogger<AccountController> _logger;
+        private readonly IMapper _mapper;
+
+        public AccountController(UserManager<Users> userManager,
+            SignInManager<Users> signInManager,
+            ILogger<AccountController> logger,
+            IMapper mapper)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginUserDTO userDTO)
+        {
+            _logger.LogInformation($"Login Attempt for {userDTO.Email} ");
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                var result = await _signInManager.PasswordSignInAsync(userDTO.Email, userDTO.Password, false, false);
+
+                if (!result.Succeeded)
+                {
+                    return Ok(false);
+                }
+
+                return Ok(userDTO);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Something went wrong in the {nameof(Login)}");
+                return Problem($"Something went wrong in the {nameof(Login)}", statusCode: 500);
+            }
+        }
+    }
+}
