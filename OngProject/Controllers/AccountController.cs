601f6d7fd3d0496a779ce4a054f2b9e0d90e6910
Magali Medina <magalimedina96@gmail.com>
2022-08-23 17:19:04 -0300

    Se modifico login

--- OngProject/Controllers/AccountController.cs
+++ OngProject/Controllers/AccountController.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Logging;
 using OngProject.Core.Models;
 using OngProject.Core.Models.DTOs;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace OngProject.Controllers
@@ -16,45 +17,29 @@ namespace OngProject.Controllers
     {
         private readonly UserManager<Users> _userManager;
         private readonly SignInManager<Users> _signInManager;
-        private readonly ILogger<AccountController> _logger;
         private readonly IMapper _mapper;
 
         public AccountController(UserManager<Users> userManager,
             SignInManager<Users> signInManager,
-            ILogger<AccountController> logger,
             IMapper mapper)
         {
             _userManager = userManager;
             _signInManager = signInManager;
-            _logger = logger;
             _mapper = mapper;
         }
 
         [HttpPost]
-        [Route("login")]
-        public async Task<IActionResult> Login([FromBody] LoginUserDTO userDTO)
+        public async Task<ActionResult<Users>> Login(LoginUserDTO login)
         {
-            _logger.LogInformation($"Login Attempt for {userDTO.Email} ");
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-            try
-            {
-                var result = await _signInManager.PasswordSignInAsync(userDTO.Email, userDTO.Password, false, false);
+            var user = _userManager.Users.SingleOrDefault(x => x.Email == login.Email);
 
-                if (!result.Succeeded)
-                {
-                    return Ok(false);
-                }
+            if (user == null) return Unauthorized("Username or password incorrect");
 
-                return Ok(userDTO);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, $"Something went wrong in the {nameof(Login)}");
-                return Problem($"Something went wrong in the {nameof(Login)}", statusCode: 500);
-            }
+            var result = await _signInManager.CheckPasswordSignInAsync(user, login.Password, false);
+
+            if (!result.Succeeded) return Unauthorized();
+
+            return Ok(user);
         }
     }
 }
