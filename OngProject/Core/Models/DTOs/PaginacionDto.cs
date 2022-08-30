b788c3b22ab2485439dcc43a675a1eb187d498b9
MatiasAcostaa <matiasacostatuc@gmail.com>
2022-08-29 22:04:15 -0300

    paginacion de miembros

---
+++ OngProject/Core/Models/DTOs/PaginacionDto.cs
@@ -0,0 +1,20 @@
+﻿namespace OngProject.Core.Models.DTOs
+{
+    public class PaginacionDto
+    {
+        public int Pagina { get; set; } = 1;
+
+        private int cantidadRegistroPorPagina { get; set; } = 10;
+        private readonly int cantidadMaximaRegistroPorPagina = 50;
+        
+        public int CantidadRegistroPorPagina
+        {
+            get => cantidadRegistroPorPagina;
+
+            set
+            {
+                cantidadRegistroPorPagina = (value > cantidadMaximaRegistroPorPagina) ? cantidadMaximaRegistroPorPagina : value;
+            }
+        }
+    }
+}
