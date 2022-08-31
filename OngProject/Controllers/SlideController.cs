74820db008179d28463fa713f04ac07a276a7e8e
Arturo-Castro <castro.ah91@gmail.com>
2022-08-31 16:43:37 -0300

    Add controller structure for Slide

---
+++ OngProject/Controllers/SlideController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OngProject.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SlideController : Controller
+    {
+        
+    }
+}
