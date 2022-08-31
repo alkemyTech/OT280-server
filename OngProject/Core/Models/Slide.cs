41c07c65e762e08a0a5782af93a682ca978e268c
Arturo-Castro <castro.ah91@gmail.com>
2022-08-31 16:26:21 -0300

    Add new migration and new model
    Added Slide model and updated database

---
+++ OngProject/Core/Models/Slide.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OngProject.Core.Models
+{
+    public class Slide
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int SlideId { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Text { get; set; }
+
+        public int Order { get; set; }
+
+        [ForeignKey("Organization")]
+        public int OrganizationId { get; set; }
+
+        public Organization Organization { get; set; }
+    }
+}
---
+++ OngProject/Migrations/20220831192105_AddSlideModel.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OngProject.Migrations
+{
+    public partial class AddSlideModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Slides",
+                columns: table => new
+                {
+                    SlideId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ImageUrl = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Text = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Order = table.Column<int>(type: "int", nullable: false),
+                    OrganizationId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Slides", x => x.SlideId);
+                    table.ForeignKey(
+                        name: "FK_Slides_Organization_OrganizationId",
+                        column: x => x.OrganizationId,
+                        principalTable: "Organization",
+                        principalColumn: "OrganizationId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Slides_OrganizationId",
+                table: "Slides",
+                column: "OrganizationId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Slides");
+        }
+    }
+}
