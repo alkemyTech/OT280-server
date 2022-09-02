e9e4904f6dad369ff122965f635080ac07a78200
Arturo-Castro <castro.ah91@gmail.com>
2022-09-02 17:55:13 -0300

    Add role based authorization for this controller

--- OngProject/Controllers/TestimonialController.cs
+++ OngProject/Controllers/TestimonialController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using OngProject.Core.Models;
@@ -10,6 +11,7 @@ using System.Threading.Tasks;
 
 namespace OngProject.Controllers
 {
+    [Authorize(Roles = "Administrator")]
     [ApiController]
     public class TestimonialController : ControllerBase
     {
