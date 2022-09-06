5c06016e98c4aa63f5980a611b9292d677c697f2
Diego Balderrama <diego.balde.job@gmail.com>
2022-09-06 00:07:20 -0300

    89 - 157 - Comments updated Model added GetAll endpoint

---
+++ OngProject/Migrations/20220906013134_CommentsFixedFixed.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OngProject.Migrations
+{
+    public partial class CommentsFixedFixed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date_Create",
+                table: "Comments",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date_Create",
+                table: "Comments");
+        }
+    }
+}
