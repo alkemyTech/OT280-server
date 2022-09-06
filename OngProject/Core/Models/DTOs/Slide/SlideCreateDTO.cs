fa0ca13aa7fe580bcf53e0f43dff0f9a0f9f9812
Olivera Leonardo <oliveraleonardoezequiel@gmail.com>
2022-09-05 23:55:42 -0300

    edit phone in organization

--- OngProject/Core/Models/DTOs/Slide/SlideCreateDTO.cs
+++ OngProject/Core/Models/DTOs/Slide/SlideCreateDTO.cs
@@ -17,9 +17,9 @@ namespace OngProject.Core.Models.DTOs.Slide
 
         public int Order { get; set; }
 
-        //[ForeignKey("Organization")]
-        //public int OrganizationId { get; set; }
+        [ForeignKey("Organization")]
+        public int OrganizationId { get; set; }
 
-        //public Organization Organization { get; set; }
+        public Organization Organization { get; set; }
     }
 }
