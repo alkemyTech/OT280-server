57359c72dba7cc64cede8472f7ebc683d3242fad
Olivera Leonardo Ezequiel <lolivera@enacom.gob.ar>
2022-09-07 15:02:15 -0300

    Add Contacts models and migration

---
+++ OngProject/Core/Models/Contact.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace OngProject.Core.Models
+{
+    public class Contact
+    {
+        /// <summary>
+        /// Criterios de aceptación:
+        /// Al completar el Formulario de Contacto, se almacenará en la base de datos.
+        /// Nombre de tabla: contacts.
+        /// Contendrá los campos 
+        /// name, 
+        /// phone, 
+        /// email, 
+        /// message, 
+        /// deletedAt (utilizada para soft delete)
+        /// </summary>
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CategoryId { get; set; }
+
+        [Required(ErrorMessage = "El campo Name es requerido")]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "El campo Phone es requerido")]
+        public int Phone { get; set; }
+
+        [Required(ErrorMessage = "El campo Email es requerido")]
+        [StringLength(50)]
+        [EmailAddress]
+        public int Email { get; set; }
+
+        [Required(ErrorMessage = "El campo Message es requerido")]
+        [StringLength(200)]
+        public string Message { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ OngProject/Migrations/20220907175634_ContactModel.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OngProject.Migrations
+{
+    public partial class ContactModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Phone = table.Column<int>(type: "int", nullable: false),
+                    Email = table.Column<int>(type: "int", maxLength: 50, nullable: false),
+                    Message = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.CategoryId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacts");
+        }
+    }
+}
