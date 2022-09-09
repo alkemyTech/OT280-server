fadd9aa71daeff692708989da145d1842f36ee20
Diego Balderrama <diego.balde.job@gmail.com>
2022-09-08 23:03:49 -0300

    57 - News Endpoint Delete

---
+++ OngProject/Migrations/20220909015838_NewModelUpdated.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OngProject.Migrations
+{
+    public partial class NewModelUpdated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "News",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "News");
+        }
+    }
+}
