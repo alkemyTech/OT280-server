307ec63eaac1172b201f83a9c8eb66cbeb2803d7
Arturo Castro <69820855+Arturo-Castro@users.noreply.github.com>
2022-09-12 15:39:12 -0300

    Post feature for news controller (#69)
    * Add pagination for news controller
    
    * Refactor fixes
    
    * Add get by id for news controller
    
    * Add post feature for news controller

---
+++ OngProject/Core/Models/DTOs/CreateNewsDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OngProject.Core.Models.DTOs
+{
+    public class CreateNewsDTO
+    {
+        [Required(ErrorMessage = "El campo Name es requerido")]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string Content { get; set; }
+
+        [Required(ErrorMessage = "El campo Image es requerido")]
+        [StringLength(100)]
+        public string Image { get; set; }
+        
+        public int CategoryId { get; set; }
+    }
+}
