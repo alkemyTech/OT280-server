f63583dee858034e9dc6799f9f8b20861ce9e0fc
Leonardo Olivera <olezdev@gmail.com>
2022-09-21 15:03:22 -0300

    ContactModel-Edit-ContactID

---
+++ OngProject/Migrations/20220921132152_ContactEditId.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OngProject.Migrations
+{
+    public partial class ContactEditId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CategoryId",
+                table: "Contacts",
+                newName: "ContactId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ContactId",
+                table: "Contacts",
+                newName: "CategoryId");
+        }
+    }
+}
